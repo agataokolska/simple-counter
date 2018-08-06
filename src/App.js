f52c860201ae576b0d182a9e4437a94a81f4b54d
agata okolska <agata.okolska@gmail.com>
2018-08-06 02:03:59 +0200

    delete test component, change version of react due to problems with babel module missing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Counter from './Counter'
-import Test from './Test'
+
 
 const App = () => (
   <Router>
     <div>
-      <Route path={'/test'} component={Test}/>
-      <Route path={'/counter'} component={()=><Counter number={0} />} />
+      <Route path={'/'} component={()=><Counter number={0} />} />
     </div>
   </Router>
 )
